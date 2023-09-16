35f7290145975d181058cb7c2b44c840b3a122a5
Biker <106655599+Habibrehman01@users.noreply.github.com>
2023-09-16 21:49:49 +0500

    Fivem Premium Loading Screen
    Fivem Premium Loading Screen.
    
    Clean
    Elegant
    Simple

---
+++ bk_loadingscreen/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game {'gta5'}
+author 'Biker#0001'
+description 'bk Loading Screen'
+version '1.0.0'
+loadscreen 'ui/index.html'
+loadscreen_cursor 'yes'
+files { 
+		'ui/index.html',
+		'ui/*.css',
+		'ui/*.js'
+	}
---
+++ bk_loadingscreen/ui/script.js
@@ -0,0 +1,26 @@
+VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+		max: 15,
+		speed: 200,
+		glare: true,
+		"max-glare": 1,
+		transition: true,
+		perspective: 1000,
+	});
+
+VanillaTilt.init(document.querySelector(".glassmorphic-card2"), {
+		max: 15,
+		speed: 200,
+		glare: true,
+		"max-glare": 1,
+		transition: true,
+		perspective: 1000,
+	});
+
+VanillaTilt.init(document.querySelector(".glassmorphic-card3"), {
+		max: 15,
+		speed: 200,
+		glare: true,
+		"max-glare": 1,
+		transition: true,
+		perspective: 1000,
+	});
